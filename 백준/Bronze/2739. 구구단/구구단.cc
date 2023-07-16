3d116fb196a0982ae9fb8449ceedc2658855efe5
codingssagae <137710022+codingssagae@users.noreply.github.com>
2023-07-16 22:06:44 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, a;
+	cin >> a;
+	for ( i = 1; i <10 ; i++)
+	{
+		cout << a << " " << '*' << " " << i << " " << '=' << " " << a * i << endl;
+	}
+}
+
+
