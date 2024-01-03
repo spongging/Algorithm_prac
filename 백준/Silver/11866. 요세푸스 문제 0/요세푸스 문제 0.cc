29e5fc07ef52fb32b63b89a928ba27680ea92b4d
codingssagae <137710022+codingssagae@users.noreply.github.com>
2024-01-04 02:06:06 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N, K; cin >> N >> K;
+    queue<int>q;
+    for (int i = 1; i <= N; i++)
+    { 
+        q.push(i);
+    }
+    cout << "<";
+    while (!q.empty())
+    {
+        for (int i = 1; i < K; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        if (q.size() != 1)
+        {
+            cout << ", ";
+        }
+        q.pop();
+    }
+    cout << ">" << "\n";
+    return 0;
+}
