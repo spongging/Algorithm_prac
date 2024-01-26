dcf49828d3219eca778abf94403d8873bb5b7535
codingssagae <137710022+codingssagae@users.noreply.github.com>
2024-01-27 00:27:35 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 72 ms, Memory: 3836 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
@@ -0,0 +1,67 @@
+#include <iostream> 
+#include <vector>
+#include <climits>
+using namespace std;
+
+vector<int>tree;
+vector<pair<int, int>>nums; // define that (idx, value)
+int N, M, h = 1;
+
+int comp(int idx1, int idx2) { // comparing value and return that index
+	if (nums[idx1].second <= nums[idx2].second) return idx1;
+	else return idx2;
+}
+
+void update(int idx, int idx2) {
+	idx += h;
+	tree[idx] = idx2;
+	while (idx > 1) {
+		idx /= 2;
+		tree[idx] = comp(tree[idx * 2], tree[idx * 2 + 1]);
+	}
+}
+
+int query(int L, int R, int node, int nodeL, int nodeR) {
+	if (L <= nodeL && R >= nodeR) return tree[node];
+	if (L > nodeR || R < nodeL) return 0;
+	int mid = (nodeL + nodeR) / 2;
+	return comp(query(L, R, node * 2, nodeL, mid), query(L, R, node * 2 + 1, mid + 1, nodeR));
+}
+
+int main(void){
+
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);  
+
+	cin >> N;
+
+	while (N > h)h <<= 1;
+	nums.resize(N + 1);
+	tree.resize(h * 2);
+	nums[0] = make_pair(0, INT_MAX); // [0] = INT_MAX
+	fill(tree.begin(), tree.end(), 0); // filling a tree node into idx 0
+
+	for (int i = 0; i < N; i++) // updating a tree
+	{
+		int num; cin >> num;
+		nums[i + 1] = make_pair(i + 1, num);
+		update(i, i + 1);
+	}
+	
+	cin >> M; 
+	for (int i = 0; i < M; i++) //starting a query
+	{
+		int a, b, c; cin >> a >> b >> c;
+		if (a == 1) {
+			nums[b].second = c;
+			update(b - 1, b);
+		}
+		if (a == 2) {
+			cout << query(b, c, 1, 1, h) << "\n";
+		}
+	}
+
+	return 0;
+}
+
