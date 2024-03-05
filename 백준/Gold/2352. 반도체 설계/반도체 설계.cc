c128a04146216efc5c304bc0e05fb76f479288f2
codingssagae <137710022+codingssagae@users.noreply.github.com>
2024-03-06 00:20:12 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 6440 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+int arr[1000001];
+vector<int>result;
+
+int main(void) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i];
+
+	int j = 0;
+	int i = 1;
+	result.push_back(arr[0]);
+
+	while (i < N) 
+	{
+		if (result[j] >= arr[i])
+		{
+			int idx = lower_bound(result.begin(), result.end(), arr[i]) - result.begin();
+			result[idx] = arr[i];
+		}
+		else
+		{
+			result.push_back(arr[i]);
+			j++;
+		}
+		i++;
+	}
+
+	//printing
+	cout << j + 1 << "\n";
+	
+	return 0;
+} 
