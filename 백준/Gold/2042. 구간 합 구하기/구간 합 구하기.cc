4a22faffe13f36e1c1009fc0c739cb6a21b9c936
이건희 <137710022+codingssagae@users.noreply.github.com>
2024-03-27 20:53:49 +0900

    [Gold I] Title: 구간 합 구하기, Time: 228 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,51 +1,62 @@
 #include <iostream>
 #include <vector>
-using namespace std; 
-typedef long long ll;   
-vector<int>nums; vector<ll>tree;
-int N, M, K; int h = 1;
+#include <algorithm>
+using namespace std;
+typedef long long ll;
 
-void update(int idx, ll value) { // update the each node
-	idx += h;
-	tree[idx] = value;
-	while(idx > 1) {
-		idx /= 2;
-		tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
-	}
+ll N, M, K;
+vector<ll>v, tree;
+
+ll sum(int i) {
+    int idx = i;
+    ll ans = 0;
+    while (idx > 0) {
+        ans += tree[idx];
+        idx -= idx & -idx;
+    }
+    return ans;
 }
 
-ll query(int L, int R, int node, int nodeL, int nodeR) { // get section sum 
-	
-	if (L > nodeR || R < nodeL) return 0;
-	if (L <= nodeL && R >= nodeR) return tree[node];
-	int mid = (nodeL + nodeR) / 2;
-	return query(L, R, node * 2, nodeL, mid) + query(L, R, node * 2 + 1, mid + 1, nodeR);
+void update(int i, ll diff) {
+    int idx = i;
+    while (idx <= tree.size()) {
+        tree[idx] += diff;
+        idx += idx & -idx;
+    }
 }
 
-int main(void){
+int main() {
 
-	ios_base::sync_with_stdio(false); 
-	cin.tie(NULL); 
-	cout.tie(NULL);  
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	cin >> N >> M >> K;  
-	
-	while (N > h) h <<= 1; // use shift for finding h
-	nums.resize(N + 1); // resize nums array and tree size
-	tree.resize(2 * h);
-	fill(tree.begin(), tree.end(), 0); // init all node of tree to zero
+    cin >> N >> M >> K;
+    v.resize(N + 5);
+    tree.resize(N + 5);
 
-	for (int i = 0; i < N; i++) //updating a tree
-	{
-		ll num; cin >> num; 
-		update(i, num);
-	}
-	for (int i = N + 1; i < N + M + K + 1; i++) // starting a query
-	{
-		int a, b; ll c; cin >> a >> b >> c; 
-		if (a == 1) update(b - 1, c);
-		if (a == 2) cout << query(b, c , 1, 1, h) << "\n";
-	}
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> v[i];
+        update(i, v[i]);
+    }
 
-	return 0;
+    M += K;
+
+    while (M--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == 1) {
+            ll diff = c - v[b];
+            v[b] = c;
+            update(b, diff);
+        }
+        else {
+            cout << sum(c) - sum(b - 1) << "\n";
+        }
+    }
+
+    return 0;
 }
