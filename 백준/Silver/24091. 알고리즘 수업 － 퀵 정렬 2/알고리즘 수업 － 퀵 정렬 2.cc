fed48e1b56c36e87cf9ef38ecb9e3b302ae76ef1
이건희 <137710022+codingssagae@users.noreply.github.com>
2024-03-27 16:02:15 +0900

    [Silver V] Title: 알고리즘 수업 - 퀵 정렬 2, Time: 100 ms, Memory: 2196 KB -BaekjoonHub

---
+++ 백준/Silver/24091. 알고리즘 수업 － 퀵 정렬 2/알고리즘 수업 － 퀵 정렬 2.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int N, K;
+int cnt = 0;
+
+
+int partition(int arr[], int p, int r) {
+    int x = arr[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++)
+    {
+        if (arr[j] <= x) {
+            i++;
+            int tmp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = tmp;
+            cnt++; 
+            if (cnt == K)
+            {
+                for (int k = 0; k < N; k++) {
+                    cout << arr[k] << " ";
+                }
+            }
+        }
+    }
+    if (i + 1 != r) {
+        int tmp = arr[r];
+        arr[r] = arr[i + 1];
+        arr[i + 1] = tmp; 
+        cnt++;
+        if (cnt == K)
+        {
+            for (int k = 0; k < N; k++) {
+                cout << arr[k] << " ";
+            }
+        }
+    }
+    return i + 1;
+}
+void quick_sort(int arr[], int p, int r) {
+    if (p < r) {
+        int q = partition(arr, p, r);
+        if (q == -1) {
+            return;
+        }
+        quick_sort(arr, p, q-1);
+        quick_sort(arr, q + 1, r);
+    }
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> K;
+    int* arr = new int[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        int num; cin >> num;
+        arr[i] = num;
+    }
+    quick_sort(arr, 0, N - 1);
+    if (cnt < K) cout << -1;
+   
+    return 0;
+}
