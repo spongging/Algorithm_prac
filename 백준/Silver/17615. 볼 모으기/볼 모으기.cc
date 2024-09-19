4144f81bbafde38e981bec8bae43040d7c7169a5
이건희 <137710022+codingssagae@users.noreply.github.com>
2024-09-19 23:35:12 +0900

    [Silver I] Title: 볼 모으기, Time: 4 ms, Memory: 2912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.cc
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std; 
 int main(void) {
 
@@ -9,14 +10,20 @@ int main(void) {
 	int N; cin >> N;
 	string s; cin >> s; 
 	int lastred = 0; int lastblue = 0; 
+	int initred = INT_MAX; int initblue = INT_MAX; 
 	for (int i = 0; i < N; i++)
 	{
 		if (s[i] == 'R') {
+			initred = min(initred, i); 
 			lastred = i;
 		}
-		else lastblue = i;
+		else {
+			lastblue = i;
+			initblue = min(initblue, i);
+		}
 	}
 	int red = 0; int blue = 0; 
+	int rightresult, leftresult; 
 	for (int i = 0; i <= lastblue; i++)
 	{
 		if (s[i] == 'R') {
@@ -30,6 +37,21 @@ int main(void) {
 			blue++; 
 		}
 	}
-	cout << min(blue, red) << "\n";
+	rightresult = min(red, blue);  
+	red = 0; blue = 0;
+	for (int i = N-1; i >= initred; i--)
+	{
+		if (s[i] == 'B') {
+			blue++;
+		}
+	}
+	for (int i = N-1; i >= initblue; i--)
+	{
+		if (s[i] == 'R') {
+			red++;
+		}
+	}
+	leftresult = min(red, blue);
+	cout << min(rightresult, leftresult) << "\n";
 	return 0;
 }
